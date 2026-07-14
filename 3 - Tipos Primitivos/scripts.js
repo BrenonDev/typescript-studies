9370e72f1c32960370980b21ee2c8579e3badbcb
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-07-14 12:58:28 -0300

    tipos primitivos

---
+++ 3 - Tipos Primitivos/scripts.js
@@ -0,0 +1,19 @@
+"use strict";
+console.log("=== TIPOS PRIMITIVOS ===");
+// Os tipos primitivos são os tipos de dados mais básicos do TypeScript. Eles incluem:
+// · number: representa números, tanto inteiros quanto decimais;
+// · string: representa cadeias de caracteres, como texto;
+// · boolean: representa valores lógicos, verdadeiro (true) ou falso (false);
+// STRING
+let username;
+username = "Brenon";
+// username = 25; // Error ao tentar trocar o valor para outro tipo de dado
+// username = true; // Error ao tentar trocar o valor para outro tipo de dado
+// NUMBER
+let total;
+total = 7;
+total = 5.5;
+// BOOLEAN
+let isLoading;
+isLoading = true;
+// ======================================================================
---
+++ 3 - Tipos Primitivos/scripts.ts
@@ -0,0 +1,31 @@
+console.log("=== TIPOS PRIMITIVOS ===")
+
+// Os tipos primitivos são os tipos de dados mais básicos do TypeScript. Eles incluem:
+
+// · number: representa números, tanto inteiros quanto decimais;
+// · string: representa cadeias de caracteres, como texto;
+// · boolean: representa valores lógicos, verdadeiro (true) ou falso (false);
+
+// STRING
+
+let username: string;
+
+username = "Brenon";
+// username = 25; // Error ao tentar trocar o valor para outro tipo de dado
+// username = true; // Error ao tentar trocar o valor para outro tipo de dado
+
+// NUMBER
+
+let total: number;
+
+total = 7;
+total = 5.5;
+
+// BOOLEAN
+
+let isLoading: boolean;
+
+isLoading = true;
+
+
+// ======================================================================
