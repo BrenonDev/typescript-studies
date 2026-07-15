668a1aa3c7708ff7cecbcba5eddd46ccfb7bc2c9
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-07-15 13:49:08 -0300

    tipos personalizados

---
+++ 4 - Tipos Personalizados/scripts.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("=== TIPOS PERSONALIZADOS ===");
+// Os tipos personalizados são uma forma de criar tipos de dados específicos para o seu projeto, permitindo maior controle e segurança no código. Eles podem ser criados usando interfaces, tipos literais, enums e classes.
+// ======================================================================
---
+++ 4 - Tipos Personalizados/scripts.ts
@@ -0,0 +1,6 @@
+console.log("=== TIPOS PERSONALIZADOS ===");
+
+// Os tipos personalizados são uma forma de criar tipos de dados específicos para o seu projeto, permitindo maior controle e segurança no código. Eles podem ser criados usando interfaces, tipos literais, enums e classes.
+
+
+// ======================================================================
