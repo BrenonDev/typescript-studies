00dc2f48f5bb9e0e372abdb821abce5eaaf0979e
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-07-16 13:50:13 -0300

    como estender interfaces

--- 4 - Tipos Personalizados/scripts.js
+++ 4 - Tipos Personalizados/scripts.js
@@ -9,3 +9,16 @@ function newProduct(product) {
 ;
 newProduct({ id: 1, name: "Produto X" });
 // ======================================================================
+console.log("=== COMO ESTENDER INTERFACES ===");
+;
+;
+;
+let teacher = { id: 1, name: "Brenon", subjects: ["JavaScript", "TypeScript"] };
+let student = { id: 2, name: "João", age: 21 };
+console.log(teacher.id);
+console.log(teacher.name);
+console.log(teacher.subjects);
+console.log(student.id);
+console.log(student.name);
+console.log(student.age);
+// ======================================================================
--- 4 - Tipos Personalizados/scripts.ts
+++ 4 - Tipos Personalizados/scripts.ts
@@ -8,6 +8,8 @@ console.log("=== TIPOS PERSONALIZADOS ===");
 
 console.log("=== CONHECENDO INTERFACE NO TYPESCRIPT ===");
 
+// Interfaces são uma forma de definir a estrutura de um objeto, especificando quais propriedades e métodos ele deve ter. Elas são úteis para garantir que os objetos sigam um contrato específico, facilitando a manutenção e a legibilidade do código.
+
 interface IProduct {
     id: number,
     name: string,
@@ -18,6 +20,43 @@ function newProduct(product: IProduct) {
 };
 
 newProduct({id: 1, name: "Produto X"});
+
+
+// ======================================================================
+
+
+console.log("=== COMO ESTENDER INTERFACES ===");
+
+// A extensão de interfaces permite criar novas interfaces com base em interfaces existentes, adicionando ou modificando propriedades e métodos. Isso promove a reutilização de código e a criação de hierarquias de tipos mais complexas.
+
+interface IPerson {
+    id: number,
+    name: string,
+};
+
+interface ITeacher extends IPerson {
+    // id: number,
+    // name: string,
+    subjects: string[],
+};
+
+interface IStudent extends IPerson {
+    // id: number,
+    // name: string,
+    age: number,
+};
+
+let teacher: ITeacher = { id: 1, name: "Brenon", subjects: ["JavaScript", "TypeScript"] };
+
+let student: IStudent = { id: 2, name: "João", age: 21 };
+
+console.log(teacher.id);
+console.log(teacher.name);
+console.log(teacher.subjects);
+
+console.log(student.id);
+console.log(student.name);
+console.log(student.age);
 
 
 // ======================================================================
