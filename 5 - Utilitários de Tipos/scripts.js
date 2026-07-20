a3e6178696043c5356cbd4e9e674cc19891fc8ea
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-07-20 13:32:25 -0300

    utilitários do TypeScript

---
+++ 5 - Utilitários de Tipos/scripts.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("=== UTILITÁRIOS DE TIPOS ===");
+// Os utilitários de tipos são tipos genéricos que permitem manipular outros tipos de forma mais flexível e poderosa. Eles são amplamente utilizados para criar tipos derivados, transformar tipos existentes e fornecer funcionalidades adicionais ao trabalhar com tipos em TypeScript.
+// ======================================================================
---
+++ 5 - Utilitários de Tipos/scripts.ts
@@ -0,0 +1,6 @@
+console.log("=== UTILITÁRIOS DE TIPOS ===");
+
+// Os utilitários de tipos são tipos genéricos que permitem manipular outros tipos de forma mais flexível e poderosa. Eles são amplamente utilizados para criar tipos derivados, transformar tipos existentes e fornecer funcionalidades adicionais ao trabalhar com tipos em TypeScript.
+
+
+// ======================================================================
